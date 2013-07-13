9d332eab26b1dc670c35b1059c6b94bd1d36565f
stormgrab <stormgrab@gmail.com>
2013-07-13 13:35:26 +0530

    Rails 3 blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '7d3a789e0b77e569fe34911c4e21848abecf7277531a1b02892051d8093596288a5b8718e22f80a7097172fb4a07e0618a0b1c05533ae9cbe22d7d2cde430041'
